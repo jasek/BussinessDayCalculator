a4764ad6c8700c4531e407d04819af4a60c6fe33
Tomasz Jaśkiewicz <jasek2@gmail.com>
2018-10-28 20:30:58 +1100

    Initial commit

---
+++ DayCounterUtils/BusinessDayCounter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DayCounterUtils
+{
+    public class BusinessDayCounter
+    {
+        public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
+        {
+            var span = secondDate - firstDate;
+            return span.Days;
+        }
+        public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime>
+       publicHolidays)
+        {
+            return 0;
+        }
+    }
+}
---
+++ DayCounterUtilsTests/BusinessDayCounterTests.cs
@@ -0,0 +1,25 @@
+using System;
+using DayCounterUtils;
+using Xunit;
+
+namespace DayCounterUtilsTests
+{
+    public class BusinessDayCounterTests
+    {
+        private readonly BusinessDayCounter businessDayCounter;
+
+        public BusinessDayCounterTests()
+        {
+            businessDayCounter = new BusinessDayCounter();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var firstDate = new DateTime();
+            var secondDate = new DateTime();
+            var result = businessDayCounter.WeekdaysBetweenTwoDates(firstDate, secondDate);
+
+        }
+    }
+}
