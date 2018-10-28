2c589307a647964128f5e5a082023f5db5953c49
Tomasz Jaśkiewicz <jasek2@gmail.com>
2018-10-28 21:11:06 +1100

    task1: all test cases

--- DayCounterUtilsTests/BusinessDayCounterTests.cs
+++ DayCounterUtilsTests/BusinessDayCounterTests.cs
@@ -13,14 +13,18 @@ namespace DayCounterUtilsTests
             businessDayCounter = new BusinessDayCounter();
         }
 
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(new[] { 2013, 10, 7 }, new[] { 2013, 10, 9 }, 1)]
+        [InlineData(new[] { 2013, 10, 5 }, new[] { 2013, 10, 14 }, 5)]
+        [InlineData(new[] { 2013, 10, 7 }, new[] { 2014, 1, 1 }, 61)]
+        [InlineData(new[] { 2013, 10, 7 }, new[] { 2013, 10, 5 }, 0)]
+        public void Test1(int[] firstDataSet, int[] secondDataSet, int expected)
         {
-            var firstDate = new DateTime(2013, 10, 7);
-            var secondDate = new DateTime(2013, 10, 9);
+            var firstDate = new DateTime(firstDataSet[0], firstDataSet[1], firstDataSet[2]);
+            var secondDate = new DateTime(secondDataSet[0], secondDataSet[1], secondDataSet[2]);
             var result = businessDayCounter.WeekdaysBetweenTwoDates(firstDate, secondDate);
 
-            Assert.Equal(1, result);
+            Assert.Equal(expected, result);
         }
     }
 }
